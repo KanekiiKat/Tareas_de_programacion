f790d5eaed52b92ef310e927b9f3f767e72526a8
Kaneki <50421371+KanekiiKat@users.noreply.github.com>
2024-10-18 09:33:51 +0200

    Create Duplicate_Encode.py

---
+++ Duplicate_Encode.py
@@ -0,0 +1,10 @@
+def duplicate_encode(word):
+    word = word.lower()
+    resultado = ""
+    for letra in word:
+        if word.count(letra) == 1:
+            resultado += "("
+        else:
+            resultado += ")"
+    
+    return resultado
